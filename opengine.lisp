c9b807067802f378949ab8a2d59ef8fe7eb2d44c
Isoraqathedh Zorethan <isoraqathedh.zorethan@gmail.com>
2017-05-10 22:20:15 +0100

    Make everything depend on an object.

--- opengine.lisp
+++ opengine.lisp
@@ -4,24 +4,48 @@
 
 (defclass opengine-instance ()
   ((program :reader program)
-   (main-stack :accessor main-stack)
-   (other-stack :accessor other-stack))
+   (main-stack :accessor main-stack
+               :initform (make-string 0))
+   (other-stack :accessor other-stack
+                :initform (make-string 0)))
   (:documentation "Object representing an opengine instance."))
-(defvar *current-instance* "")
+(defvar *current-instance*)
 
 (defun append-to-main-stack (character)
-  (setf *main-stack* (concatenate 'string *main-stack* (string character))))
+  "Add a character to the main stack."
+  (setf (main-stack *current-instance*)
+        (concatenate 'string (main-stack *current-instance*)
+                     (string character))))
 (defun append-to-other-stack (character)
-  (setf *other-stack* (concatenate 'string *other-stack* (string character)))
-  (when (find *other-stack* nil  :key #'car)
-    (append-to-main-stack (get-match *other-stack*))))
+  "Add a character to the other stack.
+
+After this, check if the current "
+  (setf (other-stack *current-instance*)
+        (concatenate 'string (other-stack *current-instance*)
+                     (string character)))
+  (let ((found-match (assoc (other-stack *current-instance*)
+                            (assoc :other-stack-matches (program *current-instance*)))))
+    (when found-match
+      (append-to-main-stack (cdr found-match)))))
+
 (defun modify-main-stack (character)
-  (setf (aref *main-stack* (1- (length *main-stack*))) character))
+  "Change the last character of the main stack with something else."
+  (setf (aref (main-stack *current-instance*)
+              (1- (length *main-stack*)))
+        character))
+
 (defun delete-main-stack ()
-  (setf ()))
-(defun flush-and-exit (&optional kill)
+  "Delete the last character of the main stack."
+  (setf (main-stack *current-instance*)
+        (subseq (main-stack *current-instance*)
+                0 (- (length (main-stack *current-instance*)) 1))))
+(defun delete-other-stack ()
+  "Delete the last character of the other stack."
+  (setf (other-stack *current-instance*)
+        (subseq (other-stack *current-instance*)
+                0 (- (length (other-stack *current-instance*)) 1))))
+
+(defun flush ()
   (uiop:run-program '("xclip" "-selection" "c")
                     :input (make-string-input-stream *main-stack*))
-  (setf *main-stack* "")
-  (when kill
-    (uiop:die 0)))
+  (setf (main-stack *current-instance*) ""))
