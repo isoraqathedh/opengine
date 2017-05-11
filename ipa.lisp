a88ca88d1a3e96cbac1b05cae19d98a8c6b791a4
Isoraqathedh Zorethan <isoraqathedh.zorethan@gmail.com>
2017-05-11 12:42:57 +0100

    Construct a test program file for use later.

---
+++ ipa.lisp
@@ -0,0 +1,10 @@
+((:other-stack-matches
+  ;; other stack matches here
+  )
+ (:timeout . 750)
+ (:keys
+  ("a" (#\a "Open Front Vowel")
+       (#\ɑ "Open Back Vowel")
+       (#\æ "Near-Open Front Vowel")
+       (#\ɐ "Near-Open Central Vowel")
+       (#\ɒ "Rounded Open Back Vowel"))))
