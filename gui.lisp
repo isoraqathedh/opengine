c26eb5a9fb7c38f6c9d4a5af429680a0bd3cba32
Isoraqathedh Zorethan <isoraqathedh.zorethan@gmail.com>
2017-05-13 22:35:37 +0100

    Remove stub function from example

--- gui.lisp
+++ gui.lisp
@@ -46,15 +46,6 @@
     (q+:add-layout layout main-stack-assembly)
     (q+:add-layout layout button-panel)))
 
-(defun timestamp (&optional (universal-time (get-universal-time)))
-  (multiple-value-bind (s m h dd mm yy day) (decode-universal-time universal-time)
-    (format NIL "~[Monday~;Tuesday~;Wednesday~;Thursday~;Friday~;Saturday~;Sunday~] ~
-                 the ~d~[st~;nd~;rd~:;th~] ~
-                 of ~[January~;February~;March~;April~;May~;June~;July~;August~;September~;October~;November~;December~] ~
-                 ~d, ~
-                 ~2,'0d:~2,'0d:~2,'0d"
-            day dd (1- (mod dd 10)) (1- mm) yy h m s)))
-
 (define-slot (main button-pressed) ()
   (declare (connected change-stack (released)))
   (setf (q+:text current-stack)
